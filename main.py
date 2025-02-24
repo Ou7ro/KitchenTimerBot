6c63d9d8eab7d26891143052f441960d02830ef7
Ou7ro <126232792+Ou7ro@users.noreply.github.com>
2025-02-24 16:40:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ from pytimeparse import parse
 from dotenv import load_dotenv
 
 
-load_dotenv('token.env')
-TOKEN = os.environ['BOT_API_TOKEN']
-TG_CHAT_ID = os.environ['TG_CHAT_ID']
-BOT = ptbot.Bot(TOKEN)
-
-
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
     percent = "{0:.1f}"
@@ -23,31 +17,35 @@ def notify_progress(secs_left, message_id, total_time):
     messege = (
         f"Осталось секунд: {secs_left}\n{render_progressbar(total_time, total_time - secs_left)}"
         )
-    BOT.update_message(TG_CHAT_ID, message_id, messege)
+    BOT.update_message(tg_chat_id, message_id, messege)
 
 
 def end_messege(author_id):
     end_messege = "Время вышло"
-    BOT.send_message(author_id, end_messege)
+    bot.send_message(author_id, end_messege)
     message = 'На сколько поставить таймер?'
-    BOT.send_message(author_id, message)
+    bot.send_message(author_id, message)
 
 
 def start_messege(author_id):
     start_messege = "Бот запущен!\nНа сколько поставить таймер?"
-    BOT.send_message(author_id, start_messege)
+    bot.send_message(author_id, start_messege)
 
 
 def timer(chat_id, time):
-    messege_id = BOT.send_message(chat_id, "Запускаю таймер")
-    BOT.create_countdown(parse(time), notify_progress, message_id=messege_id, total_time=parse(time))
-    BOT.create_timer(parse(time), end_messege, author_id=TG_CHAT_ID)
+    messege_id = bot.send_message(chat_id, "Запускаю таймер")
+    bot.create_countdown(parse(time), notify_progress, message_id=messege_id, total_time=parse(time))
+    bot.create_timer(parse(time), end_messege, author_id=tg_chat_id)
 
 
 def main():
-    start_messege(TG_CHAT_ID)
-    BOT.reply_on_message(timer)
-    BOT.run_bot()
+    load_dotenv('token.env')
+    token = os.environ['BOT_API_TOKEN']
+    tg_chat_id = os.environ['TG_CHAT_ID']
+    bot = ptbot.Bot(token)
+    start_messege(tg_chat_id)
+    bot.reply_on_message(timer)
+    bot.run_bot()
 
 
 if __name__ == '__main__':
